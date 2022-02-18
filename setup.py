b2353dd0ef1da476e941f611fb0a130ae33dc79b
Peter Bryan <peterbbryan@gmail.com>
2022-02-17 21:33:40 -0500

    chore: add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(name='wordle-bechmark',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='Peter Bryan',
+      author_email='peterbbryan@gmail.com',
+      packages=setuptools.find_packages(),
+     )
