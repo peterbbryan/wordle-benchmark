ab4a2d488f0e437b77f849884fc9b3cd3eb1b8a2
Peter Bryan <peterbbryan@gmail.com>
2022-02-17 21:37:13 -0500

    style: fix formatting

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 import setuptools
 
-setuptools.setup(name='wordle-bechmark',
-      version='1.0',
-      description='Python Distribution Utilities',
-      author='Peter Bryan',
-      author_email='peterbbryan@gmail.com',
-      packages=setuptools.find_packages(),
-     )
+setuptools.setup(
+    name="wordle-bechmark",
+    version="1.0",
+    description="Python Distribution Utilities",
+    author="Peter Bryan",
+    author_email="peterbbryan@gmail.com",
+    packages=setuptools.find_packages(),
+)
